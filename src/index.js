5cb2e1701ce95e61e7fe8d4364412f4863537d17
Camila7nunes <camila.paula@stone.com.br>
2023-10-02 14:27:29 -0300

    Remoção do .js que estavam dando conflito

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//importando server que foi exportado no Server.ts
-const Server_1 = require("./server/Server");
-//"escuta" o servidor, passando a porta que vai rodar a API node
-Server_1.server.listen(process.env.PORT || 3333, () => {
-    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
