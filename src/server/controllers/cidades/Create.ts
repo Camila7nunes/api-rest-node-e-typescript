91819497c0944390181b6b0c6a55c9f6a6ae7754
Camila7nunes <camila.paula@stone.com.br>
2023-10-04 16:47:38 -0300

    Tipagem de entrada

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 
-export const create = (req: Request, res: Response) => {
-    const data = req.body;
-    console.log(data);
+interface ICidade {
+    nome: string;
+}
+
+export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+    const data:ICidade = req.body;
+    console.log(data.nome);
     return res.send('Create');
 };
